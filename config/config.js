1e073bfadc2850ddf12613d27942016f800291bd
Amine TABOU <amin.tabou@gmail.com>
2019-03-14 22:01:48 +0100

    First changes

---
+++ config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    //MongoDB configuration
+    development: {
+        db: 'mongodb://127.0.0.1/graphql',
+        app: {
+            name: 'graphql'
+        }
+    },
+    production: {
+        // mongodb://heroku_d82tz725:gm05ehm7j8pipbl1due9qkf2f2@ds239911.mlab.com:39911/heroku_d82tz725
+        db: 'mongodb://heroku_d82tz725:gm05ehm7j8pipbl1due9qkf2f2@ds239911.mlab.com:39911/heroku_d82tz725',
+        app: {
+            name: 'graphql'
+        }
+    }
+};
