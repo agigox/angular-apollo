5e87eeb4791f9900d847b1addb4fe0bd6fc0f038
Amine TABOU <amin.tabou@gmail.com>
2019-03-14 23:43:37 +0100

    Changing temporarly the config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     //MongoDB configuration
     development: {
-        db: 'mongodb://127.0.0.1/graphql',
+        db: 'mongodb://heroku_d82tz725:gm05ehm7j8pipbl1due9qkf2f2@ds239911.mlab.com:39911/heroku_d82tz725',
         app: {
             name: 'graphql'
         }
