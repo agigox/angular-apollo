5f0325a8f0f2722abfaa4e26b4cba8fb2f2d6a2d
Amine TABOU <amin.tabou@gmail.com>
2019-03-15 01:24:58 +0100

    Updating

--- config/config.js
+++ config/config.js
@@ -7,7 +7,6 @@ module.exports = {
         }
     },
     production: {
-        // mongodb://heroku_d82tz725:gm05ehm7j8pipbl1due9qkf2f2@ds239911.mlab.com:39911/heroku_d82tz725
         db: 'mongodb://heroku_d82tz725:gm05ehm7j8pipbl1due9qkf2f2@ds239911.mlab.com:39911/heroku_d82tz725',
         app: {
             name: 'graphql'
